018b04fe699b48feebc6ce9828a47b374ab91397
jk27 <jcarlosmartin27@gmail.com>
2022-05-11 08:47:28 +0100

    Implemented pagination

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -6,7 +6,7 @@ exports.getAllTeams = async (req, res) => {
 	try {
 		// FUNCTIONALITY => FILTERING
 		// DOES => Creates a new object containing all the key-value pairs from the query parameters.
-		const queryObj = { ...req.query };
+		const queryObj = {...req.query};
 		// DOES => Removes all the fields in the array from the queryObj.
 		const excludedFields = ["page", "sort", "limit", "fields"];
 		excludedFields.forEach(el => delete queryObj[el]);
@@ -23,6 +23,26 @@ exports.getAllTeams = async (req, res) => {
 			query = query.sort(sortBy);
 		} else {
 			query = query.sort("market");
+		}
+
+		// FUNCTIONALITY => FIELD LIMITING
+		if (req.query.fields) {
+			const fields = req.query.fields.split(",").join(" ");
+			query = query.select(fields);
+		} else {
+			query = query.select("-reference");
+		}
+
+		// FUNCTIONALITY => PAGINATION
+		const page = req.query.page * 1 || 1;
+		const limit = req.query.limit * 1 || 3;
+		const skip = (page - 1) * limit;
+
+		query = query.skip(skip).limit(limit);
+
+		if (req.query.page) {
+			const numTeams = await Team.countDocuments();
+			if (skip >= numTeams) throw new Error('This page does not exist.')
 		}
 
 		// DOES => Executes the query.
