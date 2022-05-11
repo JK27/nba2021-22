5b090101d4e75374a5837674797cda8e28db06a8
jk27 <jcarlosmartin27@gmail.com>
2022-05-11 09:11:22 +0100

    Refactored API features

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -1,52 +1,15 @@
 const Team = require("./../models/teamModel");
+const APIFeatures = require('./../utils/apiFeatures')
 
 /////////////////////////////////////////////////////////// ROUTE HANDLERS
 /////////////////////////////////////// GET ALL TEAMS
 exports.getAllTeams = async (req, res) => {
 	try {
-		// FUNCTIONALITY => FILTERING
-		// DOES => Creates a new object containing all the key-value pairs from the query parameters.
-		const queryObj = {...req.query};
-		// DOES => Removes all the fields in the array from the queryObj.
-		const excludedFields = ["page", "sort", "limit", "fields"];
-		excludedFields.forEach(el => delete queryObj[el]);
-		// DOES => Advance filtering. Allows to use greater and lower than operators adding a '$' to the query to match the mongoDB operators.
-		let queryStr = JSON.stringify(queryObj);
-		queryStr = queryStr.replace(/\b(gte?|lte?)\b/g, match => `$${match}`);
-
-		// DOES => Builds the query.
-		let query = Team.find(JSON.parse(queryStr));
-
-		// FUNCTIONALITY => SORTING
-		if (req.query.sort) {
-			const sortBy = req.query.sort.split(",").join(" ");
-			query = query.sort(sortBy);
-		} else {
-			query = query.sort("market");
-		}
-
-		// FUNCTIONALITY => FIELD LIMITING
-		if (req.query.fields) {
-			const fields = req.query.fields.split(",").join(" ");
-			query = query.select(fields);
-		} else {
-			query = query.select("-reference");
-		}
-
-		// FUNCTIONALITY => PAGINATION
-		const page = req.query.page * 1 || 1;
-		const limit = req.query.limit * 1 || 3;
-		const skip = (page - 1) * limit;
-
-		query = query.skip(skip).limit(limit);
-
-		if (req.query.page) {
-			const numTeams = await Team.countDocuments();
-			if (skip >= numTeams) throw new Error('This page does not exist.')
-		}
-
 		// DOES => Executes the query.
-		const teams = await query;
+		// NOTE => Chaining methods is possible because after calling each method, we always return 'this'.
+		const features = new APIFeatures(Team.find(), req.query).filter().sort().limitFields().paginate();
+		const teams = await features.query;
+
 		// DOES => Sends the response.
 		res.status(200).json({
 			status: "success",
---
+++ utils/apiFeatures.js
@@ -0,0 +1,58 @@
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  // FUNCTIONALITY => FILTERING
+  // DOES => Creates a new object containing all the key-value pairs from the query parameters.
+  filter() {
+    const queryObj = {...this.queryString};
+    // DOES => Removes all the fields in the array from the queryObj.
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach(el => delete queryObj[el]);
+    // DOES => Advance filtering. Allows to use greater and lower than operators adding a '$' to the query to match the mongoDB operators.
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte?|lte?)\b/g, match => `$${match}`);
+
+    // DOES => Builds the query.
+    this.query = this.query.find(JSON.parse(queryStr));
+    // NOTE => We need to return the entire object in order to be able to chain other methods.
+    return this;
+  }
+
+  // FUNCTIONALITY => SORTING
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("market");
+    }
+    return this;
+  }
+
+  // FUNCTIONALITY => FIELD LIMITING
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(" ");
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select("-reference");
+    }
+    return this;
+  }
+
+  // FUNCTIONALITY => PAGINATION
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 30;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+
+}
+module.exports = APIFeatures
