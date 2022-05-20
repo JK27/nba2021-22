2b89e610e413be597fcc89cceb249fdfb3f5bf73
jk27 <jcarlosmartin27@gmail.com>
2022-05-20 12:02:13 +0100

    Implemented rate limit and installed helmet for setting security HTTP headers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require('helmet')
 
 const AppError = require("./utils/appError")
 const golbalErrorHandler = require("./controllers/errorController")
@@ -9,14 +11,79 @@ const teamRouter = require("./routes/teamRoutes");
 const playerRouter = require("./routes/playerRoutes");
 
 const app = express();
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
 
-/////////////////////////////////////////////////////////// MIDDLEWARES
+/////////////////////////////////////////////////////////// GLOBAL MIDDLEWARES
+//////////////////////////////////////////// HELMET
+// DOES => Sets security HTTP headers.
+app.use(helmet({
+	contentSecurityPolicy: {
+		directives: {
+			defaultSrc: ["'self'", "data:", "blob:", "https:", "ws:"],
+			baseUri: ["'self'"],
+			fontSrc: ["'self'", "https:", "data:"],
+			scriptSrc: [
+				"'self'",
+				"https:",
+				"http:",
+				"blob:",
+				"https://*.mapbox.com",
+				"https://js.stripe.com",
+				"https://m.stripe.network",
+				"https://*.cloudflare.com",
+			],
+			frameSrc: ["'self'", "https://js.stripe.com"],
+			objectSrc: ["'none'"],
+			styleSrc: ["'self'", "https:", "'unsafe-inline'"],
+			workerSrc: [
+				"'self'",
+				"data:",
+				"blob:",
+				"https://*.tiles.mapbox.com",
+				"https://api.mapbox.com",
+				"https://events.mapbox.com",
+				"https://m.stripe.network",
+			],
+			childSrc: ["'self'", "blob:"],
+			imgSrc: ["'self'", "data:", "blob:"],
+			formAction: ["'self'"],
+			connectSrc: [
+				"'self'",
+				"data:",
+				"blob:",
+				"https://*.stripe.com",
+				"https://*.mapbox.com",
+				"https://*.cloudflare.com/",
+				"https://bundle.js:*",
+				"ws://127.0.0.1:*/",
+			],
+		},
+	},
+}))
+
+//////////////////////////////////////////// MORGAN
 // DOES => Only uses Morgan for logging purposes when on development.
 if (process.env.NODE_ENV === "development") {
 	app.use(morgan("dev"));
 }
+//////////////////////////////////////////// RATE LIMITER
+// DOES => Rate limiter only will allow a maximum of 1000 request per hour coming from the same IP address.
+const limiter = rateLimit({
+	max: 1000,
+	windowMs: 60 * 60 * 1000,
+	message: 'Too many requests from the same IP address. Please try again in one hour.'
+});
+
+app.use('/api/', limiter);
+
+//////////////////////////////////////////// BODY PARSER
+// DOES => Reads data from the body into req.body.
+app.use(express.json({
+	limit: '10kb'
+}));
+
+//////////////////////////////////////////// STATIC FILES
+// DOES => SErves static files from the specified path.
+app.use(express.static(`${__dirname}/public`));
 
 /////////////////////////////////////////////////////////// ROUTE HANDLERS
 app.use("/api/v1/teams", teamRouter);
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+/////////////////////////////////////////////////////////// UNCAUGHT EXCEPTIONS
+process.on("uncaughtException", err => {
+	console.log("UNCAUGHT EXCEPTION! 💥 SHUTTING DOWN...");
+	console.log(err.name, err.message);
+	// DOES => Gives the server time to finish all pending requests before exiting the application.
+	// NOTE => When dealing with an ucaught exception, we need to crash the application and restart it.
+	process.exit(1);
+});
+
+/////////////////////////////////////////////////////////// DOTENV
 // DOES => Reads variables from file and saves them into Node.js environment variables.
 dotenv.config({path: "./config.env"});
 const app = require("./app");
@@ -18,11 +28,13 @@ const server = app.listen(port, () => {
 	console.log(`App running on port ${port}...`);
 });
 
+/////////////////////////////////////////////////////////// UNHANDLED REJECTIONS
 process.on('unhandledRejection', err => {
-	console.log(err.name, err.message);
 	console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+	console.log(err.name, err.message);
 	// DOES => Gives the server time to finish all pending requests before exiting the application.
 	server.close(() => {
 		process.exit(1);
 	});
 });
+
